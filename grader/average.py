45bc4ac42ac9dbe01f2206fd4a11e53f340d7ef1
Diyorbek Jumanov <76900500+djumanov@users.noreply.github.com>
2025-06-25 15:00:47 +0500

    Create average.py

---
+++ grader/average.py
@@ -0,0 +1,7 @@
+def calculate_average(scores: list[int]) -> float:
+    """
+    O'rtacha bahoni hisoblaydi.
+    :param scores: Baholar ro'yxati
+    :return: O'rtacha baho (float)
+    """
+    return sum(scores) / len(scores)
